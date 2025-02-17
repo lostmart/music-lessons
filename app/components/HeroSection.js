d23db59880c9f84646f629db0e9e0ac0875f93b4
lostmart <martinpedraza1979@gmail.com>
2025-02-17 16:57:42 +0100

    new starts

--- app/components/HeroSection.js
+++ app/components/HeroSection.js
@@ -10,93 +10,98 @@ const HeroSection = () => {
 			textAlign="center"
 			py={6}
 			px={2}
+			component="section"
 			sx={{
-				position: "relative",
-				minHeight: "70vh",
-				display: "flex",
-				flexDirection: "column",
-				justifyContent: "start",
-				alignItems: "center",
 				backgroundColor: "#bbdefb",
+				position: "relative",
 				overflow: "hidden",
-				width: "100%",
 			}}
 			id="accueil"
 		>
 			<Box
-				sx={{ position: "absolute", left: "4%" }}
-				style={{
-					animation:
-						"floatAnimation .9s cubic-bezier(0.25, 1, 0.5, 1) infinite",
-				}}
-			>
-				<Image
-					// loader={myLoader}
-					src="/imgs/note-5.png"
-					alt="Picture of the author"
-					width={30}
-					height={30}
-				/>
-			</Box>
-			<Box
-				sx={{ position: "absolute", right: "4%" }}
-				style={{
-					animation:
-						"floatAnimation .9s cubic-bezier(0.25, 1, 0.5, 1) infinite",
-				}}
-			>
-				<Image
-					// loader={myLoader}
-					src="/imgs/note-4.png"
-					alt="Picture of the author"
-					width={30}
-					height={30}
-				/>
-			</Box>
-			<Box
 				sx={{
-					position: "absolute",
-					zIndex: "0",
-					top: "32%",
-					right: "1%",
-					display: "none",
-					"@media (min-width: 600px)": {
-						display: "block",
-					},
+					position: "relative",
+					overflow: "hidden",
+					width: "100%",
+					maxWidth: "1200px",
+					margin: "0 auto",
+					minHeight: "52dvh",
 				}}
 			>
-				<Image
-					// loader={myLoader}
-					src="/imgs/boy-guitar.png"
-					alt="Picture of the author"
-					width={230}
-					height={492}
-				/>
-			</Box>
-			<Typography
-				variant="h3"
-				fontWeight="bold"
-				sx={{ fontFamily: "Barriecito, cursive" }}
-			>
-				Apprenez la Musique Facilement
-			</Typography>
-			<Typography
-				variant="h6"
-				color="textSecondary"
-				sx={{ fontFamily: "Comic Neue, cursive", fontWeight: "bold" }}
-			>
-				Cours personnalisés de piano, de guitare et de batterie pour enfants et
-				adultes à Bouafle.
-			</Typography>
-			<Button variant="contained" color="primary" sx={{ mt: 4, mb: 5 }}>
-				<Link
-					href="mailto:martinpedraza1979@gmail.com"
-					sx={{ color: "#fff", textDecoration: "none" }}
+				<Box
+					sx={{ position: "absolute", left: "4%" }}
+					style={{
+						animation:
+							"floatAnimation .9s cubic-bezier(0.25, 1, 0.5, 1) infinite",
+					}}
 				>
-					Réservez un cours gratuit
-				</Link>
-			</Button>
-
+					<Image
+						// loader={myLoader}
+						src="/imgs/note-5.png"
+						alt="Picture of the author"
+						width={30}
+						height={30}
+					/>
+				</Box>
+				<Box
+					sx={{ position: "absolute", right: "4%" }}
+					style={{
+						animation:
+							"floatAnimation .9s cubic-bezier(0.25, 1, 0.5, 1) infinite",
+					}}
+				>
+					<Image
+						// loader={myLoader}
+						src="/imgs/note-4.png"
+						alt="Picture of the author"
+						width={30}
+						height={30}
+					/>
+				</Box>
+				<Box
+					sx={{
+						position: "absolute",
+						zIndex: "0",
+						top: "32%",
+						right: "1%",
+						display: "none",
+						"@media (min-width: 600px)": {
+							display: "block",
+						},
+					}}
+				>
+					<Image
+						// loader={myLoader}
+						src="/imgs/boy-guitar.png"
+						alt="Picture of the author"
+						width={230}
+						height={492}
+					/>
+				</Box>
+				<Typography
+					variant="h3"
+					fontWeight="bold"
+					sx={{ fontFamily: "Barriecito, cursive" }}
+				>
+					Apprenez la Musique Facilement
+				</Typography>
+				<Typography
+					variant="h6"
+					color="textSecondary"
+					sx={{ fontFamily: "Comic Neue, cursive", fontWeight: "bold" }}
+				>
+					Cours personnalisés de piano, de guitare et de batterie pour enfants
+					et adultes à Bouafle.
+				</Typography>
+				<Button variant="contained" color="primary" sx={{ mt: 4, mb: 5 }}>
+					<Link
+						href="mailto:martinpedraza1979@gmail.com"
+						sx={{ color: "#fff", textDecoration: "none" }}
+					>
+						Réservez un cours gratuit
+					</Link>
+				</Button>
+			</Box>
 			<Box
 				sx={{
 					position: "absolute",
@@ -137,18 +142,6 @@ const HeroSection = () => {
 					sizes="(max-width: 490px) 100vw, 1440px"
 				/>
 			</Box>
-
-			{/* <Box
-				sx={{
-					position: "absolute",
-					width: "100vw",
-					bottom: "-70px",
-					left: "-8px",
-					zIndex: "1",
-				}}
-			>
-				
-			</Box> */}
 		</Box>
 	)
 }

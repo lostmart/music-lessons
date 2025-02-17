9c35644762a6656c4d775a5048756d8e2b570a33
lostmart <lostmart@hotmail.com>
2025-02-17 21:02:50 +0100

    new starting

--- app/components/HeroSection.js
+++ app/components/HeroSection.js
@@ -19,6 +19,36 @@ const HeroSection = () => {
 			id="accueil"
 		>
 			<Box
+				sx={{ position: "absolute", left: "4%" }}
+				style={{
+					animation:
+						"floatAnimation .9s cubic-bezier(0.25, 1, 0.5, 1) infinite",
+				}}
+			>
+				<Image
+					// loader={myLoader}
+					src="/imgs/note-5.png"
+					alt="Picture of the author"
+					width={30}
+					height={30}
+				/>
+			</Box>
+			<Box
+				sx={{ position: "absolute", right: "4%" }}
+				style={{
+					animation:
+						"floatAnimation .9s cubic-bezier(0.25, 1, 0.5, 1) infinite",
+				}}
+			>
+				<Image
+					// loader={myLoader}
+					src="/imgs/note-4.png"
+					alt="Picture of the author"
+					width={30}
+					height={30}
+				/>
+			</Box>
+			<Box
 				sx={{
 					position: "relative",
 					overflow: "hidden",
@@ -26,44 +56,46 @@ const HeroSection = () => {
 					maxWidth: "1200px",
 					margin: "0 auto",
 					minHeight: "52dvh",
+					display: "flex",
+					justifyContent: "center",
+					alignItems: "center",
 				}}
 			>
 				<Box
-					sx={{ position: "absolute", left: "4%" }}
-					style={{
-						animation:
-							"floatAnimation .9s cubic-bezier(0.25, 1, 0.5, 1) infinite",
+					sx={{
+						width: "100%",
+						"@media (min-width: 600px)": {
+							maxWidth: "50%",
+							display: "block",
+						},
 					}}
 				>
-					<Image
-						// loader={myLoader}
-						src="/imgs/note-5.png"
-						alt="Picture of the author"
-						width={30}
-						height={30}
-					/>
-				</Box>
-				<Box
-					sx={{ position: "absolute", right: "4%" }}
-					style={{
-						animation:
-							"floatAnimation .9s cubic-bezier(0.25, 1, 0.5, 1) infinite",
-					}}
-				>
-					<Image
-						// loader={myLoader}
-						src="/imgs/note-4.png"
-						alt="Picture of the author"
-						width={30}
-						height={30}
-					/>
+					<Typography
+						variant="h3"
+						fontWeight="bold"
+						sx={{ fontFamily: "Barriecito, cursive" }}
+					>
+						Apprenez la Musique Facilement
+					</Typography>
+					<Typography
+						variant="h6"
+						color="textSecondary"
+						sx={{ fontFamily: "Comic Neue, cursive", fontWeight: "bold" }}
+					>
+						Cours personnalisés de piano, de guitare et de batterie pour enfants
+						et adultes à Bouafle.
+					</Typography>
+					<Button variant="contained" color="primary" sx={{ mt: 4, mb: 5 }}>
+						<Link
+							href="mailto:martinpedraza1979@gmail.com"
+							sx={{ color: "#fff", textDecoration: "none" }}
+						>
+							Réservez un cours gratuit
+						</Link>
+					</Button>
 				</Box>
 				<Box
 					sx={{
-						position: "absolute",
-						zIndex: "0",
-						top: "32%",
-						right: "1%",
 						display: "none",
 						"@media (min-width: 600px)": {
 							display: "block",
@@ -78,29 +110,6 @@ const HeroSection = () => {
 						height={492}
 					/>
 				</Box>
-				<Typography
-					variant="h3"
-					fontWeight="bold"
-					sx={{ fontFamily: "Barriecito, cursive" }}
-				>
-					Apprenez la Musique Facilement
-				</Typography>
-				<Typography
-					variant="h6"
-					color="textSecondary"
-					sx={{ fontFamily: "Comic Neue, cursive", fontWeight: "bold" }}
-				>
-					Cours personnalisés de piano, de guitare et de batterie pour enfants
-					et adultes à Bouafle.
-				</Typography>
-				<Button variant="contained" color="primary" sx={{ mt: 4, mb: 5 }}>
-					<Link
-						href="mailto:martinpedraza1979@gmail.com"
-						sx={{ color: "#fff", textDecoration: "none" }}
-					>
-						Réservez un cours gratuit
-					</Link>
-				</Button>
 			</Box>
 			<Box
 				sx={{
